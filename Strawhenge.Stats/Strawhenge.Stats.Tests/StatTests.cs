9403fe4fe18dec6156ae5e0f6f7ad258ad547739
Matt Bristow <strawhengestudios@gmail.com>
2024-06-02 21:06:48 +0100

    added initial stat object

---
+++ Strawhenge.Stats/Strawhenge.Stats.Tests/StatTests.cs
@@ -0,0 +1,72 @@
+﻿using Xunit;
+
+namespace Strawhenge.Stats.Tests
+{
+    public class StatTests
+    {
+        [Fact]
+        public void Value_lower_than_max_should_be_able_to_be_set()
+        {
+            var stat = new Stat(max: 100);
+
+            const int value = 50;
+            stat.Set(value);
+
+            Assert.Equal(value, stat.Value);
+        }
+
+        [Fact]
+        public void Value_greater_than_max_should_set_to_max()
+        {
+            const int max = 100;
+            var stat = new Stat(max);
+
+            stat.Set(150);
+
+            Assert.Equal(max, stat.Value);
+        }
+
+        [Fact]
+        public void Value_lower_than_0_should_set_to_0()
+        {
+            var stat = new Stat(max: 100);
+
+            stat.Set(-10);
+
+            Assert.Equal(0, stat.Value);
+        }
+
+        [Fact]
+        public void Value_should_remain_the_same_when_max_increased()
+        {
+            const int value = 100;
+            var stat = new Stat(max: 100, value);
+
+            stat.SexMax(150);
+
+            Assert.Equal(value, stat.Value);
+        }
+
+        [Fact]
+        public void Value_should_be_max_when_max_set_to_less_than_value()
+        {
+            var stat = new Stat(max: 100, value: 100);
+
+            const int max = 90;
+            stat.SexMax(max);
+
+            Assert.Equal(max, stat.Value);
+        }
+
+        [Fact]
+        public void Max_lower_than_0_should_set_to_0()
+        {
+            var stat = new Stat(max: 100, value: 100);
+
+            stat.SexMax(-30);
+
+            Assert.Equal(0, stat.Max);
+            Assert.Equal(0, stat.Value);
+        }
+    }
+}
---
+++ Strawhenge.Stats/Strawhenge.Stats/Stat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Strawhenge.Stats
+{
+    public class Stat
+    {
+        public Stat()
+        {
+        }
+
+        public Stat(int max, int value = 0)
+        {
+            Max = max;
+            Value = value;
+        }
+
+        public int Max { get; private set; }
+
+        public int Value { get; private set; }
+
+        public void Set(int value)
+        {
+            Value = Math.Min(
+                Math.Max(value, 0), Max);
+        }
+
+        public void SexMax(int max)
+        {
+            Max = Math.Max(max, 0);
+            Value = Math.Min(Value, Max);
+        }
+    }
+}
