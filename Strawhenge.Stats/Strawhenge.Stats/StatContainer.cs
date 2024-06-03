314c90d3023a1cb5c0081ad02cc254f37e0de4a2
Matt Bristow <strawhengestudios@gmail.com>
2024-06-03 12:58:00 +0100

    added stat container

---
+++ Strawhenge.Stats/Strawhenge.Stats/StatContainer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FunctionalUtilities;
+using Strawhenge.Common.Logging;
+
+namespace Strawhenge.Stats
+{
+    public class StatContainer
+    {
+        readonly Dictionary<string, Stat> _statsByName = new Dictionary<string, Stat>();
+        readonly ILogger _logger;
+
+        public StatContainer(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public IReadOnlyList<Stat> Stats => _statsByName.Values.ToArray();
+
+        public Maybe<Stat> FindStat(string name)
+        {
+            if (_statsByName.TryGetValue(name, out var stat))
+                return stat;
+
+            return Maybe.None<Stat>();
+        }
+
+        public Stat AddStat(string name, int max, int value = 0)
+        {
+            if (_statsByName.TryGetValue(name, out var stat))
+            {
+                _logger.LogError($"Stat '{name}' already exists.");
+                return stat;
+            }
+
+            stat = new Stat(name, max, value);
+            _statsByName.Add(name, stat);
+
+            return stat;
+        }
+    }
+}
